1957e16d89a10e2a6c2553c95cbfa29570cd586e
Dayver3 <dima.paa.voloshin@gmail.com>
2022-08-25 12:10:24 +0300

    new commit

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -4,5 +4,5 @@ $db = new PDO($params['db.conn'],$params['db.user'],$params['db.pass']);
 
 
 session_start();
-require_once 'C:\wamp64\www\blog-test\root\src\core\route.php';
+require_once 'C:\wamp64\www\blog-test\src\core\route.php';
 Route::start();
