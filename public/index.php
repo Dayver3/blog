b4704137a681c7043170fc445479c2e0e1bb8310
Dayver3 <dima.paa.voloshin@gmail.com>
2022-08-25 17:15:35 +0300

    new commit+

--- public/index.php
+++ public/index.php
@@ -1,9 +1,7 @@
 <?php
 
     set_include_path('C:\wamp64\www\blog-test\src\application\controllers' . PATH_SEPARATOR . 'C:\wamp64\www\blog-test\src\application\models' . PATH_SEPARATOR . 'C:\wamp64\www\blog-test\root\src\application\views' . PATH_SEPARATOR . 'C:\wamp64\www\blog-test\src\core');
-var_dump($_SERVER['REQUEST_URI']);
     const LOGIN = 'login';
-    var_dump(spl_autoload_register());
     function autoloader($class)
     {
         var_dump("{$class}.php");
